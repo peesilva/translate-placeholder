191c78e844440887da7881214c21ecc92825c2d0
Pedro Silva <arbeit.pedro.silva@gmail.com>
2023-11-06 11:41:29 -0300

    fix + upgrade

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const toggleFormButton = document.getElementById("toggleFormButton");
-    const formulario2 = document.getElementById("formulario2");
-  
-    toggleFormButton.addEventListener("click", function() {
-      if (formulario2.classList.contains("formulario-hidden")) {
-        formulario2.classList.remove("formulario-hidden");
-      } else {
-        formulario2.classList.add("formulario-hidden");
-      }
-    });
+  const formulario2 = document.getElementById("formulario2");
+  const toggleLanguage = document.getElementById("language-switch");
+
+
+  toggleLanguage.addEventListener("click", function() {
+    if (formulario2.classList.contains("formulario-hidden")) {
+      formulario2.classList.remove("formulario-hidden");
+    } else {
+      formulario2.classList.add("formulario-hidden");
+    }
   });
+});
